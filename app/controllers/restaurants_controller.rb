9aac9e6da3c93c14444db01485a11e2f2b5b1776
mordochi <lilynnchi@gmail.com>
2018-01-27 17:20:25 +0800

    add categories tab on restaurant index page using Nav-pills components

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,6 +1,7 @@
 class RestaurantsController < ApplicationController
   def index
     @restaurants = Restaurant.order(id: :desc).page(params[:page]).per(9)
+    @categories = Category.all
   end
 
   def show
