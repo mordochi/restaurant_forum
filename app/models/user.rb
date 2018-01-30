17d6e8c51a45f6043ca180ff015d4e44af6aeb44
mordochi <lilynnchi@gmail.com>
2018-01-30 23:16:02 +0800

    create like model/setup relation among user,restaurant and favorite

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,6 +7,8 @@ class User < ApplicationRecord
   has_many :restaurants, through: :comments
   has_many :favorites, dependent: :destroy
   has_many :favorited_restaurants, through: :favorites, source: :restaurant
+  has_many :likes, dependent: :destroy
+  has_many :lied_restaurants, through: :likes, source: :restaurant
   validates_presence_of :name
   mount_uploader :avatar, AvatarUploader
   
