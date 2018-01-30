50f4bf605acaee9c9d779c920619e53e5647f502
mordochi <lilynnchi@gmail.com>
2018-01-30 23:24:32 +0800

    add like and unlike action on RestaurantController

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -32,4 +32,17 @@ class RestaurantsController < ApplicationController
     redirect_back(fallback_location: root_path)
   end
 
+  def like
+    @restaurant = Restaurant.find(params[:id])
+    @restaurant.likes.create!(user: current_user)
+    redirect_back(fallback_location: root_path)  # 導回上一頁
+  end
+
+  def unlike
+    @restaurant = Restaurant.find(params[:id])
+    likes = Like.where(restaurant: @restaurant, user: current_user)
+    likes.destroy_all
+    redirect_back(fallback_location: root_path)
+  end
+
 end
