c4ed29d9d0220336793be3b83e1e16dad3d0651f
mordochi <lilynnchi@gmail.com>
2018-01-31 16:20:51 +0800

    setup routes for friend(unfriend) function

--- config/routes.rb
+++ config/routes.rb
@@ -19,6 +19,7 @@ Rails.application.routes.draw do
   resources :categories, only: :show
   resources :users, only: [:index, :show, :edit, :update]
   resources :followships, only: [:create, :destroy]
+  resources :friendships, only: [:create, :destroy]
   root "restaurants#index"
 
   namespace :admin do
