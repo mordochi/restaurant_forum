942455ed5e16829a2b008b8fcca63578e56d7cdf
mordochi <lilynnchi@gmail.com>
2018-01-31 14:24:37 +0800

    setup: user has many followers

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,6 +11,8 @@ class User < ApplicationRecord
   has_many :lied_restaurants, through: :likes, source: :restaurant
   has_many :followships, dependent: :destroy
   has_many :followings, through: :followships
+  has_many :inverse_followships, class_name: "Followship", foreign_key: "following_id"
+  has_many :followers, through: :inverse_followships, source: :user
   validates_presence_of :name
   mount_uploader :avatar, AvatarUploader
   
